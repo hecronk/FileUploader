ed740f5dc47bbf5cba785ede1826943d53747887
damir <damir_a@paycon.su>
2025-11-08 01:23:37 +0500

    added thread and process pool executors

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+
 from fastapi import FastAPI
 
 from src.routers.auth import router as auth_router
@@ -5,6 +7,18 @@ from src.routers.file import router as file_router
 from src.routers.job import router as job_router
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup_event():
+    app.state.thread_pool = ThreadPoolExecutor(max_workers=10)
+    app.state.process_pool = ProcessPoolExecutor(max_workers=4)
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    app.state.thread_pool.shutdown(wait=True)
+    app.state.process_pool.shutdown(wait=True)
+
 app.include_router(auth_router)
 app.include_router(file_router)
 app.include_router(job_router)
--- src/routers/file.py
+++ src/routers/file.py
@@ -9,6 +9,6 @@ router = APIRouter(prefix="/file", tags=["File"])
 
 @router.post("/upload")
 async def upload(upload_file: UploadFile = FileField(...), user: User = Depends(get_current_user)):
-    data = await upload_file.read()
+    data: bytes = await upload_file.read()
     job = await job_manager.create_job(data)
-    return {"job_id": job.id, "status": job.status}
+    return {"job_id": job.uuid, "status": job.status}
